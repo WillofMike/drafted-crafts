bb4cfc1f2be69f3a3435be9ed6db4d4feaf35d39
Mike <you@example.com>
2017-09-22 14:58:33 -0600

    gutted the original formatting and started with the pictures again

--- public/app.js
+++ public/app.js
@@ -1,27 +1,9 @@
-$("#wood-container").click(function () {
-  if ( $( "#wood-section-2" ).is( ":hidden" ) ) {
-    $( "#wood-section-2" ).slideDown( "slow" );
+$(".wood-container").click(function () {
+  if ( $( "#wood3" ).is( ":hidden" ) ) {
+    $( "#wood3" ).slideDown( "slow" );
 
   } else {
-    $( "#wood-section-2" ).hide('');
-
-  }
-});
-$("#craft-container").click(function () {
-  if ( $( "#craft-section-2" ).is( ":hidden" ) ) {
-    $( "#craft-section-2" ).slideDown( "slow" );
-
-  } else {
-    $( "#craft-section-2" ).hide('');
-
-  }
-});
-$("#laser-container").click(function () {
-  if ( $( "#laser-section-2" ).is( ":hidden" ) ) {
-    $( "#laser-section-2" ).slideDown( "slow" );
-
-  } else {
-    $( "#laser-section-2" ).hide('');
+    $( "#wood3" ).hide('#wood');
 
   }
 });
