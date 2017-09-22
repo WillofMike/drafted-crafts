40159ed41153ccce538875ff82ab3a1e5ad19f6b
Mike <you@example.com>
2017-09-21 23:26:54 -0600

    added click actions

--- public/app.js
+++ public/app.js
@@ -1,1 +1,27 @@
-console.log("hello");
+$("#wood-container").click(function () {
+  if ( $( "#wood-section-2" ).is( ":hidden" ) ) {
+    $( "#wood-section-2" ).slideDown( "slow" );
+
+  } else {
+    $( "#wood-section-2" ).hide('');
+
+  }
+});
+$("#craft-container").click(function () {
+  if ( $( "#craft-section-2" ).is( ":hidden" ) ) {
+    $( "#craft-section-2" ).slideDown( "slow" );
+
+  } else {
+    $( "#craft-section-2" ).hide('');
+
+  }
+});
+$("#laser-container").click(function () {
+  if ( $( "#laser-section-2" ).is( ":hidden" ) ) {
+    $( "#laser-section-2" ).slideDown( "slow" );
+
+  } else {
+    $( "#laser-section-2" ).hide('');
+
+  }
+});
